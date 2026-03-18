6f53f58aa6417e001e193a1c222f00e5915a09b9
Marco Ricca <marco.ricca@covisian.com>
2026-03-18 09:51:17 +0100

    updated script for create 1 xcframework only

--- Package.swift
+++ Package.swift
@@ -1,6 +1,8 @@
 // swift-tools-version: 5.9
 import PackageDescription
 
+// This Package.swift is automatically updated by GitHub Actions during release.
+
 let package = Package(
     name: "SCXSocketIO",
     platforms: [
@@ -10,19 +12,13 @@ let package = Package(
         .watchOS(.v6)
     ],
     products: [
-        .library(name: "SCXSocketIO",   targets: ["SCXSocketIO"]),
-        .library(name: "SCXStarscream", targets: ["SCXStarscream"]),
+        .library(name: "SCXSocketIO", targets: ["SCXSocketIO"]),
     ],
     targets: [
         .binaryTarget(
-            name: "SCXStarscream",
-            url: "https://github.com/smile-cx/sio-ios-scx/releases/download/v15.2.0/SCXStarscream.xcframework.zip",
-            checksum: "c137025319151f986ea77938d20b93988e8d0fb78535ddebf829d48fcf2a7aba"
-        ),
-        .binaryTarget(
             name: "SCXSocketIO",
             url: "https://github.com/smile-cx/sio-ios-scx/releases/download/v15.2.0/SCXSocketIO.xcframework.zip",
-            checksum: "62cc758131b8e71173faac62e10a5f007d87202098a13810e0dac6c559e9f605"
+            checksum: "PLACEHOLDER_WILL_BE_REPLACED_BY_CI"
         ),
     ]
 )
